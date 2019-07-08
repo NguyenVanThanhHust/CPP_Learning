99bc9af3423c947a9aeb4c3c03dec0a578bd764c
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-07-08 12:59:20 +0700

    "add const.cpp"

--- CPP_Project/CPP_Project/Advance_Cpp/const.cpp
+++ CPP_Project/CPP_Project/Advance_Cpp/const.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+// const : a complie time constraint that an object can not be modified
 int main()
 {
+    const int i = 9;
 
+    const int *p1 = &i;// data is const, pointer is not
+    cout << "pointer to i:" << p1 << endl;
+    p1++;
+
+    int* const p2 = 0; // pointer to integer is const, data is not
+
+    const int* const p3 = &i; // both data and pointer are const
+
+    // If const is on the left of *, data is const
+    // If const is on the right of *, pointer is const
+    cout << "I value: " << i << endl;
+    cout << "pointer to i increased: " << p1 << endl;
+    cout << "const pointer p2: " << p2 << endl;
+    return 0;
 }
