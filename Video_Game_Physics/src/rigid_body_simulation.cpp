2553d6d143422bba04941c79e4d7e70ac4209b73
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2023-08-24 18:40:48 +0700

    rename and declare rigid body simulation

---
+++ Video_Game_Physics/src/rigid_body_simulation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <thread>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define NUM_PARTICLES 2
+
+void RunSimulation()
+{
+
+};
+
+int main()
+{
+    RunSimulation();
+    return 0;
+}
